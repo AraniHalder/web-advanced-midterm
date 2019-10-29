ce731c93edeaa2752f7bfada88421553aeac0086
AraniHalder <31628640+AraniHalder@users.noreply.github.com>
2019-10-29 08:15:56 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,287 @@
+var video = document.getElementById("myVideo");
+var video = document.getElementById("myVideo2");
+var video = document.getElementById("myVideo3");
+
+document.getElementById("blue").style.transitionDuration = "0.4s";
+document.getElementById("darkblue").style.transitionDuration = "0.4s";
+document.getElementById("blueline").style.transitionDuration = "0.4s";
+document.getElementById("blueline2").style.transitionDuration = "0.4s";
+
+document.getElementById("myVideo").style.transitionDuration = "0.4s";
+document.getElementById("myVideo2").style.transitionDuration = "0.4s";
+document.getElementById("myVideo3").style.transitionDuration = "0.4s";
+
+document.getElementById("pink").style.transitionDuration = "0.4s";
+document.getElementById("pinkline").style.transitionDuration = "0.4s";
+document.getElementById("pinkline2").style.transitionDuration = "0.4s";
+document.getElementById("darkpink").style.transitionDuration = "0.4s";
+
+document.getElementById("yellow").style.transitionDuration = "0.4s";
+document.getElementById("yellowline").style.transitionDuration = "0.4s";
+document.getElementById("yellowline2").style.transitionDuration = "0.4s";
+document.getElementById("darkyellow").style.transitionDuration = "0.4s";
+
+document.getElementById("blue2").style.transitionDuration = "0.4s";
+document.getElementById("darkblue2").style.transitionDuration = "0.4s";
+document.getElementById("blueline21").style.transitionDuration = "0.4s";
+document.getElementById("blueline22").style.transitionDuration = "0.4s";
+
+document.getElementById("pink2").style.transitionDuration = "0.4s";
+document.getElementById("pinkline2").style.transitionDuration = "0.4s";
+document.getElementById("pinkline21").style.transitionDuration = "0.4s";
+document.getElementById("darkpink22").style.transitionDuration = "0.4s";
+
+document.getElementById("yellow2").style.transitionDuration = "0.4s";
+document.getElementById("yellowline2").style.transitionDuration = "0.4s";
+document.getElementById("yellowline21").style.transitionDuration = "0.4s";
+document.getElementById("darkyellow22").style.transitionDuration = "0.4s";
+
+function PinkFunction() {
+document.getElementById("blue").style.width = "8vw";
+document.getElementById("darkblue").style.width = "0vw";
+document.getElementById("darkblue").style.opacity  = "0";
+document.getElementById("blueline").style.left  = "6.5vw";
+document.getElementById("blueline2").style.left  = "4vw";
+document.getElementById("myVideo").style.opacity  = "0";
+document.getElementById("myVideo").style.left  = "-84vw";
+
+document.getElementById("pink").style.width = "92vw";
+document.getElementById("pinkline").style.left  = "90.5%";
+document.getElementById("pinkline2").style.left  = "88%";
+document.getElementById("myVideo2").style.opacity  = "1";
+document.getElementById("myVideo2").style.left  = "8.1vw";
+document.getElementById("darkpink").style.opacity  = "1";
+document.getElementById("darkpink").style.left  = "8vw";
+document.getElementById("darkpink").style.width  = "84vw";
+
+document.getElementById("darkyellow").style.opacity  = "0";
+document.getElementById("myVideo3").style.opacity  = "0";
+
+document.getElementById("blue2").style.height = "10%";
+document.getElementById("blue2").style.top = "90%";
+document.getElementById("darkblue2").style.height = "0vw";
+document.getElementById("darkblue2").style.opacity  = "0";
+document.getElementById("blueline21").style.top = "92%";
+document.getElementById("blueline22").style.top = "94%";
+document.getElementById("myVideo11").style.opacity  = "0";
+document.getElementById("myVideo11").style.top  = "80vh";
+
+document.getElementById("pink2").style.height = "100%";
+document.getElementById("pink2").style.top = "10%";
+document.getElementById("pinkline21").style.top  = "12%";
+document.getElementById("pinkline22").style.top  = "14%";
+document.getElementById("myVideo12").style.opacity  = "1";
+document.getElementById("myVideo12").style.top  = "40%";
+document.getElementById("darkpink2").style.opacity  = "1";
+document.getElementById("darkpink2").style.top  = "0vw";
+document.getElementById("darkpink2").style.height  = "50vw";
+
+document.getElementById("darkyellow2").style.opacity  = "0";
+document.getElementById("myVideo13").style.opacity  = "0";
+}
+
+function BlueFunction() {
+document.getElementById("blue").style.width = "84vw";
+document.getElementById("darkblue").style.width = "84vw";
+document.getElementById("darkblue").style.opacity  = "1";
+document.getElementById("blueline").style.left  = "82.5%";
+document.getElementById("blueline2").style.left  = "80%";
+document.getElementById("myVideo").style.opacity  = "1";
+document.getElementById("myVideo").style.left  = "0vw";
+
+document.getElementById("pink").style.width = "92vw";
+document.getElementById("pinkline").style.left  = "90.5%";
+document.getElementById("pinkline2").style.left  = "88%";
+document.getElementById("myVideo2").style.opacity  = "0";
+document.getElementById("myVideo2").style.left  = "8vw";
+document.getElementById("darkpink").style.opacity  = "0";
+
+document.getElementById("darkyellow").style.opacity  = "0";
+document.getElementById("myVideo3").style.opacity  = "0";
+
+document.getElementById("blue2").style.height = "80vh";
+document.getElementById("blue2").style.top = "20vh";
+document.getElementById("darkblue2").style.height = "50vw";
+document.getElementById("darkblue2").style.opacity  = "1";
+document.getElementById("blueline21").style.top = "22%";
+document.getElementById("blueline22").style.top = "24%";
+document.getElementById("myVideo11").style.opacity  = "1";
+document.getElementById("myVideo11").style.top  = "60%";
+
+document.getElementById("pink2").style.height = "92vw";
+document.getElementById("pink2").style.top = "10%";
+document.getElementById("pinkline21").style.top  = "12%";
+document.getElementById("pinkline22").style.top  = "14%";
+document.getElementById("myVideo12").style.opacity  = "0";
+document.getElementById("darkpink2").style.opacity  = "0";
+
+document.getElementById("darkyellow2").style.opacity  = "0";
+document.getElementById("myVideo13").style.opacity  = "0";
+}
+
+function YellowFunction() {
+document.getElementById("blue").style.width = "8vw";
+document.getElementById("darkblue").style.width = "0vw";
+document.getElementById("darkblue").style.opacity  = "0";
+document.getElementById("blueline").style.left  = "6.5vw";
+document.getElementById("blueline2").style.left  = "4vw";
+document.getElementById("myVideo").style.opacity  = "0";
+document.getElementById("myVideo").style.left  = "-84vw";
+
+document.getElementById("pink").style.width = "16vw";
+document.getElementById("pinkline").style.left  = "14.5vw";
+document.getElementById("pinkline2").style.left  = "12vw";
+document.getElementById("myVideo2").style.opacity  = "0";
+document.getElementById("myVideo2").style.left  = "-84vw";
+document.getElementById("darkpink").style.width  = "0vw";
+document.getElementById("darkpink").style.opacity  = "0";
+
+document.getElementById("darkyellow").style.opacity  = "1";
+document.getElementById("myVideo3").style.opacity  = "1";
+
+document.getElementById("blue2").style.height = "10%";
+document.getElementById("blue2").style.top = "90%";
+document.getElementById("darkblue2").style.height = "0vw";
+document.getElementById("darkblue2").style.opacity  = "0";
+document.getElementById("blueline21").style.top = "92%";
+document.getElementById("blueline22").style.top = "94%";
+document.getElementById("myVideo11").style.opacity  = "0";
+document.getElementById("myVideo11").style.top  = "80vh";
+
+document.getElementById("pink2").style.height = "20%";
+document.getElementById("pink2").style.top = "80%";
+document.getElementById("pinkline21").style.top  = "82%";
+document.getElementById("pinkline22").style.top  = "84%";
+document.getElementById("myVideo12").style.opacity  = "0";
+document.getElementById("myVideo12").style.top  = "90%";
+document.getElementById("darkpink").style.width  = "0vw";
+document.getElementById("darkpink").style.opacity  = "0";
+
+document.getElementById("darkyellow2").style.opacity  = "1";
+document.getElementById("myVideo13").style.opacity  = "1";
+}
+
+function PinkFunctionMobile() {
+document.getElementById("blue2").style.height = "10%";
+document.getElementById("blue2").style.top = "90%";
+document.getElementById("darkblue2").style.height = "0vw";
+document.getElementById("darkblue2").style.opacity  = "0";
+document.getElementById("blueline21").style.top = "92%";
+document.getElementById("blueline22").style.top = "94%";
+document.getElementById("myVideo11").style.opacity  = "0";
+document.getElementById("myVideo11").style.top  = "80vh";
+
+document.getElementById("pink2").style.height = "100%";
+document.getElementById("pink2").style.top = "10%";
+document.getElementById("pinkline21").style.top  = "12%";
+document.getElementById("pinkline22").style.top  = "14%";
+document.getElementById("myVideo12").style.opacity  = "1";
+document.getElementById("myVideo12").style.top  = "40%";
+document.getElementById("darkpink2").style.opacity  = "1";
+document.getElementById("darkpink2").style.top  = "0vw";
+document.getElementById("darkpink2").style.height  = "50vw";
+
+document.getElementById("darkyellow2").style.opacity  = "0";
+document.getElementById("myVideo13").style.opacity  = "0";
+
+document.getElementById("blue").style.width = "8vw";
+document.getElementById("darkblue").style.width = "0vw";
+document.getElementById("darkblue").style.opacity  = "0";
+document.getElementById("blueline").style.left  = "6.5vw";
+document.getElementById("blueline2").style.left  = "4vw";
+document.getElementById("myVideo").style.opacity  = "0";
+document.getElementById("myVideo").style.left  = "-84vw";
+
+document.getElementById("pink").style.width = "92vw";
+document.getElementById("pinkline").style.left  = "90.5%";
+document.getElementById("pinkline2").style.left  = "88%";
+document.getElementById("myVideo2").style.opacity  = "1";
+document.getElementById("myVideo2").style.left  = "8vw";
+document.getElementById("darkpink").style.opacity  = "1";
+document.getElementById("darkpink").style.left  = "8vw";
+document.getElementById("darkpink").style.width  = "84vw";
+
+document.getElementById("darkyellow").style.opacity  = "0";
+document.getElementById("myVideo3").style.opacity  = "0";
+}
+
+function BlueFunctionMobile() {
+document.getElementById("blue2").style.height = "80vh";
+document.getElementById("blue2").style.top = "20vh";
+document.getElementById("darkblue2").style.height = "50vw";
+document.getElementById("darkblue2").style.opacity  = "1";
+document.getElementById("blueline21").style.top = "22%";
+document.getElementById("blueline22").style.top = "24%";
+document.getElementById("myVideo11").style.opacity  = "1";
+document.getElementById("myVideo11").style.top  = "60%";
+
+document.getElementById("pink2").style.height = "92vw";
+document.getElementById("pink2").style.top = "10%";
+document.getElementById("pinkline21").style.top  = "12%";
+document.getElementById("pinkline22").style.top  = "14%";
+document.getElementById("myVideo12").style.opacity  = "0";
+document.getElementById("darkpink2").style.opacity  = "0";
+
+document.getElementById("darkyellow2").style.opacity  = "0";
+document.getElementById("myVideo13").style.opacity  = "0";
+
+document.getElementById("blue").style.width = "84vw";
+document.getElementById("darkblue").style.width = "84vw";
+document.getElementById("darkblue").style.opacity  = "1";
+document.getElementById("blueline").style.left  = "82.5%";
+document.getElementById("blueline2").style.left  = "80%";
+document.getElementById("myVideo").style.opacity  = "1";
+document.getElementById("myVideo").style.left  = "0vw";
+
+document.getElementById("pink").style.width = "92vw";
+document.getElementById("pinkline").style.left  = "90.5%";
+document.getElementById("pinkline2").style.left  = "88%";
+document.getElementById("myVideo2").style.opacity  = "0";
+document.getElementById("myVideo2").style.left  = "8vw";
+document.getElementById("darkpink").style.opacity  = "0";
+
+document.getElementById("darkyellow").style.opacity  = "0";
+document.getElementById("myVideo3").style.opacity  = "0";
+}
+
+function YellowFunctionMobile() {
+document.getElementById("blue2").style.height = "10%";
+document.getElementById("blue2").style.top = "90%";
+document.getElementById("darkblue2").style.height = "0vw";
+document.getElementById("darkblue2").style.opacity  = "0";
+document.getElementById("blueline21").style.top = "92%";
+document.getElementById("blueline22").style.top = "94%";
+document.getElementById("myVideo11").style.opacity  = "0";
+document.getElementById("myVideo11").style.top  = "80vh";
+
+document.getElementById("pink2").style.height = "20%";
+document.getElementById("pink2").style.top = "80%";
+document.getElementById("pinkline21").style.top  = "82%";
+document.getElementById("pinkline22").style.top  = "84%";
+document.getElementById("myVideo12").style.opacity  = "0";
+document.getElementById("myVideo12").style.top  = "90%";
+document.getElementById("darkpink").style.width  = "0vw";
+document.getElementById("darkpink").style.opacity  = "0";
+
+document.getElementById("darkyellow2").style.opacity  = "1";
+document.getElementById("myVideo13").style.opacity  = "1";
+
+document.getElementById("blue").style.width = "8vw";
+document.getElementById("darkblue").style.width = "0vw";
+document.getElementById("darkblue").style.opacity  = "0";
+document.getElementById("blueline").style.left  = "6.5vw";
+document.getElementById("blueline2").style.left  = "4vw";
+document.getElementById("myVideo").style.opacity  = "0";
+document.getElementById("myVideo").style.left  = "-84vw";
+
+document.getElementById("pink").style.width = "16vw";
+document.getElementById("pinkline").style.left  = "14.5vw";
+document.getElementById("pinkline2").style.left  = "12vw";
+document.getElementById("myVideo2").style.opacity  = "0";
+document.getElementById("myVideo2").style.left  = "-84vw";
+document.getElementById("darkpink").style.width  = "0vw";
+document.getElementById("darkpink").style.opacity  = "0";
+
+document.getElementById("darkyellow").style.opacity  = "1";
+document.getElementById("myVideo3").style.opacity  = "1";
+}
